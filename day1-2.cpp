4d5fa270a5323022c322661acd81bc5e825121db
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-01 11:50:31 +0100

    feat: day 1 ex 2 but not right answer

---
+++ day1-2.cpp
@@ -0,0 +1,44 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+  std::ifstream fich;
+  fich.open("day1.txt");
+  if (fich.is_open()) {
+    std::cout << "File opened successfully\n\n";
+  } else {
+    std::cout << "Couldn't open file\n";
+    return -1;
+  }
+
+  std::string instruction;
+  int cont = 0, lock = 50;
+  while (std::getline(fich, instruction)) {
+    if (instruction[0] == 'R') {
+      instruction.erase(0, 1);
+      int sum = std::stoi(instruction);
+      lock += sum;
+      while (lock >= 100) {
+        lock -= 100;
+        cont++;
+      }
+    } else if (instruction[0] == 'L') {
+      instruction.erase(0, 1);
+      int subs = std::stoi(instruction);
+      lock -= subs;
+      while (lock < 0) {
+        lock += 100;
+        cont++;
+      }
+    } else {
+      std::cout << "Wrong input format\n";
+      return -1;
+      cont++;
+    }
+  }
+
+  fich.close();
+  std::cout << "The number 0 ticks " << cont << " times\n";
+  return 0;
+}
