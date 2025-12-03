57da2b18f5ab2aa9655ba38f838d9ad3e6f05212
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-03 11:19:42 +0100

    feat: day3 not working on 1 specific case

---
+++ day3-java/day3.java
@@ -0,0 +1,90 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class day3 {
+  static class Pair {
+    private Integer key;
+    private Character value;
+
+    public Pair(Integer key, Character value) {
+      this.key = key;
+      this.value = value;
+    }
+
+    public Pair(Pair pair) {
+      this.key = pair.getKey();
+      this.value = pair.getValue();
+    }
+
+    public Integer getKey() {
+      return key;
+    }
+
+    public Character getValue() {
+      return value;
+    }
+
+    public void setKey(Integer key) {
+      this.key = key;
+    }
+
+    public void setValue(Character value) {
+      this.value = value;
+    }
+
+    public String toString() {
+      String pair = "Key: " + this.key + ", Value: " + this.value;
+      return pair;
+    }
+  }
+
+  public static Pair getBiggerCharPos(String line) {
+    Pair solution = new Pair(0, line.charAt(0));
+
+    for (int i = 1; i < line.length(); i++) {
+      if (line.charAt(i) > solution.getValue()) {
+        solution.setKey(i);
+        solution.setValue(line.charAt(i));
+      }
+    }
+
+    return solution;
+  };
+
+  public static void main(String[] args) {
+    File fich = new File("day3-example.txt");
+
+    try (Scanner reader = new Scanner(fich)) {
+      Integer sumTot = 0;
+
+      while (reader.hasNextLine()) {
+        String line = reader.nextLine();
+        ArrayList<Pair> numbers = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+          Pair s = new Pair(getBiggerCharPos(line));
+          numbers.add(s);
+          line = line.substring(0, s.getKey()) + (char) ('0' - 1) + line.substring(s.getKey() + 1, line.length());
+        }
+
+        numbers.sort((a, b) -> {
+          return Integer.compare(a.getKey(), b.getKey());
+        });
+
+        String numberStr = "";
+        for (Pair pair : numbers) {
+          numberStr = numberStr + pair.getValue();
+        }
+        System.out.println(numberStr);
+        sumTot += Integer.parseInt(numberStr);
+      }
+
+      System.out.println("The total sum of joltage is " + sumTot);
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found error ocurred");
+      e.printStackTrace();
+    }
+  }
+}
