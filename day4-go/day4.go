706a9bbfdd1cf25f2a977179cd9c73693cba54ba
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-04 14:54:25 +0100

    feat: done for the 4 nearest

--- day4-go/day4.go
+++ day4-go/day4.go
@@ -48,11 +48,60 @@ func main() {
 
 	for _, line := range matrix {
 		for _, num := range line {
-			print(num)
+			fmt.Print(num)
 		}
-		println()
+		fmt.Println()
 	}
 
-	println()
+	for row, rowElem := range matrix {
+		for col := range rowElem {
+			cont := 0
+
+			if matrix[row][col] == 1 {
+				switch row {
+				case 0:
+					if matrix[row+1][col] == 1 {
+						cont++
+					}
+				case len(matrix) - 1:
+					if matrix[row-1][col] == 1 {
+						cont++
+					}
+				default:
+					if matrix[row+1][col] == 1 {
+						cont++
+					}
+					if matrix[row-1][col] == 1 {
+						cont++
+					}
+				}
+
+				switch col {
+				case 0:
+					if matrix[row][col+1] == 1 {
+						cont++
+					}
+				case len(rowElem) - 1:
+					if matrix[row][col-1] == 1 {
+						cont++
+					}
+				default:
+					if matrix[row][col+1] == 1 {
+						cont++
+					}
+					if matrix[row][col-1] == 1 {
+						cont++
+					}
+				}
+
+				if cont < 4 {
+					paperN++
+					fmt.Println("Row, Col:", row, col)
+				}
+			}
+		}
+	}
+
+	fmt.Println()
 	fmt.Println("The paper rolls that can be accessed are:", paperN)
 }
