3356e7abf6ab44a4b02fdc1e26be67cb4ce5c541
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-05 14:51:15 +0100

    fix: this shi is gonna be optimized asf

--- day5-rust/src/day5.rs
+++ day5-rust/src/day5.rs
@@ -9,25 +9,23 @@ fn check_ranges_overlap(ranges: &mut [(i32, i32)]) {
 pub fn ex1() {
     let full_fich =
         fs::read_to_string("day5-example.txt").expect("Could not open the file to read");
-    let fich = full_fich.lines();
+    let fich: Vec<&str> = full_fich.split("\n\n").collect();
+    let ranges_lines = fich[0].lines();
+    let ids = fich[1].lines();
 
-    let mut flag = true;
     let mut ranges = Vec::<(i32, i32)>::new();
-    for line in fich {
-        if flag && line.is_empty() {
-            flag = false;
-            check_ranges_overlap(&mut ranges);
-            continue;
-        }
 
-        if flag {
-            let min_max: Vec<&str> = line.split("-").collect();
-            ranges.push((
-                min_max[0].parse().expect("A range is not a Number"),
-                min_max[1].parse().expect("A range is not a Number"),
-            ));
-        } else {
-            continue;
-        }
+    for line in ranges_lines {
+        let min_max: Vec<&str> = line.split("-").collect();
+        ranges.push((
+            min_max[0].parse().expect("A range is not a Number"),
+            min_max[1].parse().expect("A range is not a Number"),
+        ));
+    }
+
+    check_ranges_overlap(&mut ranges);
+
+    for id in ids {
+        println!("id:{id}");
     }
 }
