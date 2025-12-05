dc7247dfc02ad772082832c419097a338606010b
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-05 16:36:25 +0100

    feat: day 1 ex 1 done successfully and optimized as much as i can

--- day5-rust/src/day5.rs
+++ day5-rust/src/day5.rs
@@ -1,21 +1,47 @@
 use std::fs;
 
-fn check_ranges_overlap(ranges: &mut [(i32, i32)]) {
-    for r in 0..ranges.len() {
-        println!("{r}")
+fn is_between(n: i64, range: (i64, i64)) -> bool {
+    if n > range.0 && n < range.1 {
+        return true;
+    }
+    false
+}
+
+fn is_between_equals(n: i64, range: (i64, i64)) -> bool {
+    if n >= range.0 && n <= range.1 {
+        return true;
+    }
+    false
+}
+
+fn check_ranges_overlap(ranges: &mut [(i64, i64)]) {
+    for r in 0..(ranges.len() - 1) {
+        for v in (r + 1)..ranges.len() {
+            if ranges[v] != (-1, -1) {
+                if is_between(ranges[r].0, ranges[v]) && is_between(ranges[r].1, ranges[v]) {
+                    ranges[r] = ranges[v];
+                    ranges[v] = (-1, -1)
+                } else if is_between(ranges[r].0, ranges[v]) {
+                    ranges[r].0 = ranges[v].0;
+                    ranges[v] = (-1, -1)
+                } else if is_between(ranges[r].1, ranges[v]) {
+                    ranges[r].1 = ranges[v].1;
+                    ranges[v] = (-1, -1)
+                }
+            }
+        }
     }
 }
 
 pub fn ex1() {
-    let full_fich =
-        fs::read_to_string("day5-example.txt").expect("Could not open the file to read");
+    let full_fich = fs::read_to_string("src/day5.txt").expect("Could not open the file to read");
     let (ranges_fich, ids_fich) = full_fich
         .split_once("\n\n")
         .expect("Couldnt split full fich onto ranges and ids");
     let ranges_lines = ranges_fich.lines();
     let ids = ids_fich.lines();
 
-    let mut ranges = Vec::<(i32, i32)>::with_capacity(ranges_lines.clone().count());
+    let mut ranges = Vec::<(i64, i64)>::with_capacity(ranges_lines.clone().count());
 
     for line in ranges_lines {
         let (start, end) = line
@@ -29,7 +55,16 @@ pub fn ex1() {
 
     check_ranges_overlap(&mut ranges);
 
+    let mut cont = 0;
     for id in ids {
-        println!("id:{id}");
+        for r in &ranges {
+            if *r != (-1, -1) && is_between_equals(id.parse().expect("And id is not a Number"), *r)
+            {
+                cont += 1;
+                break;
+            }
+        }
     }
+
+    println!("\nThere are {cont} fresh products");
 }
