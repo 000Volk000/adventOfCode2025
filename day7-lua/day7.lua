592b6f3143c185925c09a515f48266bb0e7061bb
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-07 13:13:52 +0100

    feat: day7 ex1 done successfully

---
+++ day7-lua/day7.lua
@@ -0,0 +1,47 @@
+Explored = {}
+
+function Down_the_matrix(lines, i, index, switch)
+	if i >= #lines then
+		return 0
+	end
+
+	if Explored[i .. "," .. index] then
+		return 0
+	end
+	Explored[i .. "," .. index] = true
+
+	if switch[string.sub(lines[i], index, index)] then
+		return 1 + Down_the_matrix(lines, i + 1, index - 1, switch) + Down_the_matrix(lines, i + 1, index + 1, switch)
+	else
+		return Down_the_matrix(lines, i + 1, index, switch)
+	end
+end
+
+local fich = io.open("day7.txt", "r")
+if fich == nil then
+	error("Error opening the file")
+end
+
+local lines = {}
+local i = 1
+
+for line in fich:lines() do
+	lines[i] = line
+	i = i + 1
+end
+
+fich:close()
+
+local index = string.find(lines[1], "S")
+if index == nil then
+	error("Couldnt find the S on the first line")
+end
+table.remove(lines, 1)
+
+local switch = {
+	["."] = false,
+	["^"] = true,
+}
+local cont = Down_the_matrix(lines, 1, index, switch)
+
+print("The total number of divisions is: " .. cont)
