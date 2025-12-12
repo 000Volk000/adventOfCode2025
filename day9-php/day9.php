efaf10d7d0ba567464ae410bdd4ce32e793e2261
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-13 00:02:02 +0100

    feat: parsed points into matrix

--- day9-php/day9.php
+++ day9-php/day9.php
@@ -1,3 +1,17 @@
 <?php
 
-  echo 'Hello World!';
+function splitString($str)
+{
+    return(explode(",", $str));
+}
+
+$fich = fopen("day9-example.txt", "r") or die("Unable to open file");
+$points = array();
+while (!feof($fich)) {
+    array_push($points, fgets($fich));
+}
+array_pop($points);
+fclose($fich);
+
+$points = array_map("splitString", $points);
+print_r($points);
